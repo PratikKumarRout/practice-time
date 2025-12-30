be0a6d3e109126cff7e8d67cadf9796179a5b9bc
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-12-30 20:14:47 +0530

    new changes

---
+++ logic.js
@@ -0,0 +1,14 @@
+// challenge 1
+const multiply = (a, b) => a * b;
+
+const isEven = num => num % 2;
+
+const getGreeting = (name) => "Hello " + name;
+
+console.log(multiply(5,5));
+console.log(isEven(3));
+console.log(getGreeting("Boss"));
+
+
+
+// challenge 2
