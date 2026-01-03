896f2be7446820cb82653cdadd14611fce3926c0
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-03 16:01:39 +0530

    new practice

--- Hello.java
+++ Hello.java
@@ -1,72 +1,35 @@
+// challenge 2
 public class Hello {
+
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Tonly");
-        System.out.println(person.getName());
+        Car myCar = new Car();
+        myCar.speed = 889;
+        myCar.doors = 4;
+        myCar.showSpeed();
+        System.out.println(myCar.doors);
 
-        person.setAge(27);
-        System.out.println(person.getAge());
-
-        BankAccount myAccount = new BankAccount();
-
-        myAccount.deposit(500.0);
-        myAccount.withdraw(200.0);
-        myAccount.withdraw(400.0);
-
-        System.out.println("Final Balance: $" + myAccount.getBalance());
+        myCar.move();
+        
     }
 }
 
-class Person {
-    private String name;
-    private int age;
+class Vehicle {
+    int speed;
 
-    // getter
-    public String getName() {
-        return name;
+    void showSpeed() {
+        System.out.println(speed);
     }
 
-    // setter
-    public void setName(String newName) {
-        this.name = newName;
+    void move() {
+        System.out.println("Moving.....");
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int newAge) {
-        if (newAge > 0) {
-            this.age = newAge;
-        } else {
-            System.out.println("invalid age !");
-        }
-    }
-
 }
 
-class BankAccount {
-    private double balance;
+class Car extends Vehicle {
+    int doors;
 
-    public double getBalance() {
-        return balance;
+    void move() {
+        System.out.println("Driving on 4 wheels.....");
     }
 
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("deposited: " + amount);
-        } else {
-            System.out.println("Invalid deposit amount !");
-        }
-    }
-
-    public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            System.out.println("withdraw : " + amount);
-        } else {
-            System.out.println("Invalid withdrawal amount !");
-        }
-    }
 }
