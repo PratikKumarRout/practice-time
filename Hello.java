7c735fd1f6e9b2425cac95c6d6ecb2ba03baa6fa
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-03 18:43:23 +0530

    static practices

--- Hello.java
+++ Hello.java
@@ -1,37 +1,13 @@
-// challege 1
+// challenge 1
 public class Hello {
 
     public static void main(String[] args) {
-        Student s1 = new Student("Tony", 101);
-        Student s2 = new Student("Stark");
-        System.out.println(s1.name + s1.rollNumber);
-        System.out.println(s2.name + s2.rollNumber);
-
-        Employee e1 = new Employee(1091993);
-        System.out.println(e1.salary);
+        System.out.println(MathUtils.square(5));
     }
 }
 
-class Employee {
-    double salary;
-
-    Employee(double salary) {
-        this.salary = salary;
+class MathUtils {
+    static int square(int num) {
+        return num * num;
     }
-}
-
-class Student {
-    String name;
-    int rollNumber;
-
-    Student(String sName, int sRoll) {
-        name = sName;
-        rollNumber = sRoll;
-    }
-
-    Student(String sName) {
-        name = sName;
-        rollNumber = 0;
-    }
-
 }
