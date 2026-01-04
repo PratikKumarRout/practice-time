cb0548693d27587ec2a818041b44273419bb54bc
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-04 18:57:12 +0530

    recursion practice

--- Hello.java
+++ Hello.java
@@ -1,23 +1,14 @@
 // challenge 1
 public class Hello {
 
-    public static void main(String[] args) {
-        SlowThread thread1 = new SlowThread();
-        thread1.start();
-        System.out.println("Main thread is done.");
-    }
-}
-
-class SlowThread extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            try {
-                Thread.sleep(1000);
-                System.out.println("Background Count: " + i);
-            } catch (InterruptedException e) {
-                System.out.println("Thread was interrupted.");
-            }
+    static void countdown(int num) {
+        if (num > 0) {
+            System.out.print(num + " ");
+            countdown(num - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        countdown(10);
     }
 }
