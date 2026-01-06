c466d690302e7afd07d0f2a3e2b6c3cc72743191
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-06 19:28:56 +0530

    for of loop

--- logic.js
+++ logic.js
@@ -1,27 +1,6 @@
 // challenge 1
-const arr = [9, 8, 1, 2, 3, 3, 4, 5, 10, 5, , 5];
+const colors = ["red", "green", "blue"];
+for (const color of colors) {
+    console.log(color);
+}
 
-const removeDuplicate = (arr) => [...new Set(arr.filter(Boolean))];
-console.log(removeDuplicate(arr.sort((a, b) => a - b)));
-
-// challenge 2
-const isAnagram = (str1, str2) => {
-
-    if (str1.length !== str2.length) return false;
-
-    const count = {};
-
-    for (let char of str1.toLowerCase()) {
-        count[char] = (count[char] || 0) + 1;
-    }
-
-    for (let char of str2.toLowerCase()) {
-        if (!count[char]) return false;
-        count[char]--;
-    }
-
-    return true;
-};
-
-console.log(isAnagram("Hello", "bello"));
-console.log(isAnagram("listen", "Silent"));
