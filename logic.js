cb71c348517d2f4e5ef47fd01060d38dc485496a
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-06 19:58:31 +0530

    new practice changes in map

--- logic.js
+++ logic.js
@@ -2,3 +2,10 @@
 const fruits = new Map([["apples", 100], ["banana", 200], ["oranges", 400]]);
 
 console.log(fruits.get("apples"));
+
+const nums = [1, 2, 3];
+const doubled = nums.map(num => num * 2);
+
+console.log(doubled);
+console.log(nums);
+
