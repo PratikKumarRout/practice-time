5fe10b80313972bd7f5f766fbb86ffdc0353792d
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-06 19:43:30 +0530

    changes in loop practice

--- logic.js
+++ logic.js
@@ -1,8 +1,11 @@
 // for of loop is for array, strings, sets, maps gets the values directly
-const colors = ["red", "green", "blue"];
+const colors = ["red", "green", "blue", 1, 2, 3, 3, "blue", "yellow"];
 for (const color of colors) {
     console.log(color);
 }
+console.log(colors.sort());
+const nodups = new Set(colors);
+console.log(nodups);
 
 // for each loop is a functional style, good for doing something to every item
 colors.forEach((color, index) => {
