b311a835b2b408dcc5a8423534287e096d162d28
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-06 19:48:17 +0530

    new map practice

--- logic.js
+++ logic.js
@@ -1,20 +1,4 @@
-// for of loop is for array, strings, sets, maps gets the values directly
-const colors = ["red", "green", "blue", 1, 2, 3, 3, "blue", "yellow"];
-for (const color of colors) {
-    console.log(color);
-}
-console.log(colors.sort());
-const nodups = new Set(colors);
-console.log(nodups);
+// challenge maps
+const fruits = new Map([["apples", 100], ["banana", 200], ["oranges", 400]]);
 
-// for each loop is a functional style, good for doing something to every item
-colors.forEach((color, index) => {
-    console.log(`${index}: ${color}`);
-});
-
-// for in loop is for objects , gets the key
-const car = { brand: "Tesla", model: "x" };
-for (const key in car) {
-    console.log(key, car[key]);
-
-}
+console.log(fruits.get("apples"));
