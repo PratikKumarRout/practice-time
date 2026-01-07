e261095d33898c4d1f190d20a0949fbf687c8c36
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-07 20:22:02 +0530

    changes practice wrapper

--- logic.js
+++ logic.js
@@ -1,24 +1,11 @@
 // challenge 1
-const flipcoin = new Promise((resolve, reject) => {
-    const isHeads = Math.random() > 0.5;
-    if (isHeads) {
-        resolve("victory! it is heads");
-    } else {
-        reject("Oh no, it is tails.");
-    }
-});
-
-async function flip() {
+async function wrapper() {
     try {
-        const fp = await flipcoin;
-        console.log(fp);
-
+        await fetchData();
     } catch (error) {
-        console.log(error);
-        console.log("ohh no try again.");
-        
+        console.log("Error caught in wrapper.");
 
     }
 }
 
-flip();
+wrapper().catch(() => console.log("error caught in caller"));
