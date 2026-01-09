59d7872e0774e769660a9cedbeaa030e78c1d840
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-09 16:35:22 +0530

    practice more

--- logic.js
+++ logic.js
@@ -1,6 +1,15 @@
 // challenge 1
 const cars = ["saab", "volvo", "bmw"];
 
+cars.push("Lemon");
+
 const newcar = cars.length;
 
+cars[8] = "mango";
+
 console.log(newcar);
+
+cars.forEach(element => {
+    console.log(element);
+
+});
