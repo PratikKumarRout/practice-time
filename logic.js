6948da7b1c00e5246205ce5542394eb918012931
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-09 20:45:51 +0530

    practice huss

--- logic.js
+++ logic.js
@@ -3,6 +3,6 @@ const numbers = [10, 20, 30, 40, 50];
 
 const strings = ["apple", [["banana"], "cherry"], "date"];
 
-const full = strings.flat(Infinity);
+const removedups = numbers.splice(2, 1, "huss");
 
-console.log(full);
+console.log(numbers);
