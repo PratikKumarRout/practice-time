4aa42316bced8dfff430cb5266a02a19477670e8
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-10 18:30:47 +0530

    practice making css

--- logic.js
+++ logic.js
@@ -1,15 +1,11 @@
 // challenge 1
 const cart = [
     { name: "Laptop", price: 1200, category: "Electronics", inStock: true },
-    { name: "Mouse", price: 25, category: "Electronics", inStock: true },
+    { name: "Mouse", price: 25, category: "Electronics", inStock: true},
     { name: "Keyboard", price: 75, category: "Electronics", inStock: false },
     { name: "Notebook", price: 10, category: "Stationery", inStock: true }
 ];
 
-const totalPrice = cart.reduce((total, item) => total + item.price, 100);
+const hasItems = cart.some(item => item.price > 1000);
 
-console.log(totalPrice);
-
-const reverseNames = cart.reduceRight((acc, item) => acc + item.name + " ", " + ");
-
-console.log(reverseNames);
+console.log(hasItems);
