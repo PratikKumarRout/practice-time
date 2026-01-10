0578127fa7a911cdc81e64784e61fa3558d64a2f
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-10 15:08:23 +0530

    what to practice

--- logic.js
+++ logic.js
@@ -1,16 +1,11 @@
 // challenge 1
-const cars = [
-    { type: "Volvo", year: 2016 },
-    { type: "Saab", year: 2001 },
-    { type: "BMW", year: 2010 }
+const cart = [
+    { name: "Laptop", price: 1200, category: "Electronics", inStock: true },
+    { name: "Mouse", price: 25, category: "Electronics", inStock: true },
+    { name: "Keyboard", price: 75, category: "Electronics", inStock: false },
+    { name: "Notebook", price: 10, category: "Stationery", inStock: true }
 ];
 
-const car = cars.sort((a, b) => {
-    let x = a.type.toLowerCase();
-    let y = b.type.toLowerCase();
+const prductsNmaes = cart.map(element => element.price);
 
-    if (x < y) { return -1; }
-    if (x > y) {return 1;}
-    return 0;
-});
-console.log(car);
+console.log(prductsNmaes);
